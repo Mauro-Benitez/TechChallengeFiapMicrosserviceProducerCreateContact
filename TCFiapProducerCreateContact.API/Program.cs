8b0307a110b6ab0824b11bf4da3a01c9cdb3fbd5
caiofabiogomes <caiofabio250@gmail.com>
2025-03-18 19:55:16 -0300

    fixes

--- TCFiapProducerCreateContact.API/Program.cs
+++ TCFiapProducerCreateContact.API/Program.cs
@@ -1,6 +1,8 @@
 using MassTransit;
+using RabbitMQ.Client;
 using TCFiapProducerCreateContact.Application.Services;
 using TCFiapProducerCreateContact.Application.Services.Implementation;
+using TechChallenge.SDK.Infrastructure.Message;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,26 @@ builder.Services.AddMassTransit(x =>
     x.UsingRabbitMq((context, cfg) =>
     {
         cfg.Host(envHostRabbitMqServer);
+
+        cfg.Publish<UpdateContactMessage>(x =>
+        {
+            x.ExchangeType = ExchangeType.Direct; // Tipo do exchange do DLX
+            x.BindQueue("update-contact-dlx-exchange", "update-contact-dlx");
+        });
+
+        cfg.Publish<CreateContactMessage>(x =>
+        {
+            x.ExchangeType = ExchangeType.Direct; // Tipo do exchange do DLX
+            x.BindQueue("create-contact-dlx-exchange", "create-contact-dlx");
+        });
+
+        cfg.Publish<RemoveContactMessage>(x =>
+        {
+            x.ExchangeType = ExchangeType.Direct; // Tipo do exchange do DLX
+            x.BindQueue("delete-contact-dlx-exchange", "delete-contact-dlx");
+        });
+
+
     });
 });
 builder.Services.AddScoped<IContactService, ContactService>();
--- TCFiapProducerCreateContact.Application/Services/Implementation/RabbitMqClient.cs
+++ TCFiapProducerCreateContact.Application/Services/Implementation/RabbitMqClient.cs
@@ -25,7 +25,6 @@ namespace TCFiapProducerCreateContact.Application.Services.Implementation
         public async Task PublicMessageUpdate(UpdateContactMessage contact)
         {
             var nomeFila = "update-contact-queue";
-            await EnsureQueueExists(nomeFila, "update-contact-dlx-exchange", "update-contact-dlx");
             var endpoint = await _bus.GetSendEndpoint(new Uri($"queue:{nomeFila}"));
             await endpoint.Send(contact);
         }
@@ -34,25 +33,6 @@ namespace TCFiapProducerCreateContact.Application.Services.Implementation
             var nomeFila = "delete-contact-queue";
             var endpoint = await _bus.GetSendEndpoint(new Uri($"queue:{nomeFila}"));
             await endpoint.Send(id);
-        }
-
-        private async Task EnsureQueueExists(string queueName, string? dlxExchange = null, string? dlxRoutingKey = null)
-        {
-            var host = Environment.GetEnvironmentVariable("RABBITMQ_HOST") ?? "amqp://localhost";
-            var factory = new ConnectionFactory() { Uri = new Uri(host) };
-
-            await using var connection = await factory.CreateConnectionAsync(); // 🚀 Uso correto na versão 7.0
-            await using var channel = await connection.CreateChannelAsync(); // 🚀 Novo método na versão 7.0
-
-            var arguments = new Dictionary<string, object>();
-
-            if (!string.IsNullOrEmpty(dlxExchange) && !string.IsNullOrEmpty(dlxRoutingKey))
-            {
-                arguments["x-dead-letter-exchange"] = dlxExchange;
-                arguments["x-dead-letter-routing-key"] = dlxRoutingKey;
-            }
-
-            await channel.QueueDeclareAsync(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: arguments);
         }
 
 
